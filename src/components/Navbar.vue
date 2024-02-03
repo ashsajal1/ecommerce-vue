eec632da90ea8dcbe2990c860c757dd1616ec423
ashsajal1 <ashsajal1@gmail.com>
2024-02-03 15:26:10 +0800

    create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <header class="flex h-[60px] items-center justify-between p-2">
+        <RouterLink to="/" class="text-lg text-fuchsia-700 font-bold">Logo</RouterLink>
+        <div class="flex items-center justify-between gap-2">
+            <RouterLink to="/explore" class="btn-outline">Explore</RouterLink>
+            <RouterLink to="/signup" class="btn">Sign up</RouterLink>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
