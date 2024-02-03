b65ec4976c6c4da7feee3cf0ff5c4fe8529cf399
ashsajal1 <ashsajal1@gmail.com>
2024-02-03 15:33:43 +0800

    Create hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex items-center justify-center flex-col gap-2 p-12">
+        <h1 class="text-2xl md:text-3xl font-bold bg-gradient-to-tr from-fuchsia-700 to-amber-400 bg-clip-text text-transparent">Welcome
+            to E-Commerce shop</h1>
+        <p>Buy products with huge discounts!</p>
+        <div class="flex gap-2 mt-3">
+            <button class="btn-outline">Create an account</button>
+            <button class="btn">Explore</button>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import Hero from '@/components/Hero.vue';
+
 
 </script>
 
 <template>
   <main>
-   
+   <Hero />
   </main>
 </template>
