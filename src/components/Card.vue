a4ab82ebae1015bd5459047a618875ab5989821d
ashsajal1 <ashsajal1@gmail.com>
2024-02-03 16:05:06 +0800

    create and render card dynamically

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="border w-[200px] rounded">
+        <div class="h-[100px] w-full bg-fuchsia-200 rounded-t"></div>
+        <div>
+            <div class="flex items-center justify-between px-2">
+                <h3 class="text-lg font-medium">Title</h3>
+                <h3 class="text-lg font-medium text-amber-400">3$</h3>
+            </div>
+
+            <div class="flex gap-2 items-center p-2 justify-between w-full">
+                <button class="btn w-full">Add to cart</button>
+                <button class="btn-outline w-full">Buy</button>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import Card from '@/components/Card.vue';
 import Hero from '@/components/Hero.vue';
-
-
 </script>
 
 <template>
   <main>
-   <Hero />
+    <Hero />
+    <div class="p-2 grid grid-cols-3 md:grid-cols-6 gap-2">
+      <Card v-for="(item) in 20" :key="item" />
+    </div>
   </main>
 </template>
